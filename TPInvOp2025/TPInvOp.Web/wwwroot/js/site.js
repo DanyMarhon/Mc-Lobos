f8957f35202d00037a928f1410826a12b4f6238e
Kevin Baez <baezk501@gmail.com>
2025-07-03 15:26:54 -0300

    Reparación de diseño de la barra de nav.

--- TPInvOp2025/TPInvOp.Web/wwwroot/js/site.js
+++ TPInvOp2025/TPInvOp.Web/wwwroot/js/site.js
@@ -1,11 +1,13 @@
 ﻿const temaOscuro = () => {
     document.querySelector("body").setAttribute("data-bs-theme", "dark");
     document.querySelector("#d1-icon").setAttribute("class", "bi bi-sun-fill");
+    document.querySelector("li").setAttribute("text-light")
 }
 
 const temaClaro = () => {
     document.querySelector("body").setAttribute("data-bs-theme", "light");
     document.querySelector("#d1-icon").setAttribute("class", "bi bi-moon-fill");
+    document.querySelector("li").setAttribute("text-dark")
 }
 
 const cambiarTema = () => {
